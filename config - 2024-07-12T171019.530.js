9c66efd26a2c115b0d2a98a5117bbd3fca8a701f
Xoul ser <131429508+Xoulfff@users.noreply.github.com>
2024-07-12 17:10:36 +0530

    Add files via upload

---
+++ config - 2024-07-12T171019.530.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
+
+// Other
+global.link = 'https://chat.whatsapp.com/BxR0LrCkAVKGHriVvo7sKy'
+global.linkGroup = 'https://chat.whatsapp.com/BxR0LrCkAVKGHriVvo7sKy'
+
+global.apikey = 'bf2d2cf29b3edc604b447983';
+global.ownername = "𝐗ᴏᴜʟ 𝐒ᴇʀ";
+global.owner = ['919633605648'];
+global.premium = ['919633605648'];
+global.packname = "xᴏᴜʟ";
+global.author = "sᴇʀ";
+global.sessionName = 'session';
+global.prefa = [".", "!", "/", "?"];
+global.typemenu = "v1"
+global.menuType = "1"
+global.autoswview = true
+global.autoread = false
+global.available = false
+global.autoTyping = false
+global.autoRecord = false
+global.autoreact = false
+global.onlygroup = false
+global.onlypc = false
+global.welcome = false
+global.autoBlock = false
+ 
+/*REPLY MESSAGE*/
+global.mess = {
+    banned: '`You are *Banned* fron using commands!`',
+    bangc: '`This Group is *Banned* from using Commands!`',
+    success: '`✓ Success`',
+    admin: '`This feature is only for group admins`',
+    botAdmin: '`I am not an admin!`',
+    owner: '`You are not my owner`',
+    group: '`You can use this command only in groups ❌`',
+    private: '`Feature is used only for private chats!`',
+    bot: '`Bot number user special features`',
+    wait: '*Processing Your request*',
+    endLimit: '`Your daily limit has expired, the limit will be reset every 12 hours`',
+};
+
+global.limitawal = {
+    premium: 'Infinity',
+    free: 10
+};
